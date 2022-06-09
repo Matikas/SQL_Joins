ad3a5bde24008386691cdbcc058d4c263d1d560c
Marius Paškevičius <marius.paskevicius@yahoo.com>
2022-06-09 21:22:33 +0300

    updating script and iliustracija

--- JOINS.sql
+++ JOINS.sql
@@ -4,27 +4,33 @@ JOIN pavyzdziai: kuo skiriasi INNER, LEFT, RIGH ir FULL
 
 ***/
 
-
+--1
 select * from 
 Projektai INNER JOIN Vykdymas ON Nr = Projektas
 
+--2
 select * from 
 Projektai LEFT JOIN Vykdymas ON Nr = Projektas
 
+--3
 select * from 
 Projektai RIGHT JOIN Vykdymas ON Nr = Projektas
 
+--4
 select * from 
 Projektai FULL JOIN Vykdymas ON Nr = Projektas
 
+--5
 select * from 
 Projektai LEFT JOIN Vykdymas ON Nr = Projektas
 where Projektas IS NULL
 
+--6
 select * from 
 Projektai RIGHT JOIN Vykdymas ON Nr = Projektas
 where Projektai.NR IS NULL
 
+--7
 select * from 
 Projektai FULL JOIN Vykdymas ON Nr = Projektas
 where Vykdymas.Projektas IS NULL OR Projektai.NR IS NULL
